d5095be1e1e01c989442713ff0a1c2935af93944
Sanjay-13 <69335575+Sanjay-13@users.noreply.github.com>
2021-10-25 01:35:07 -0500

    Added welcome page

--- src/main/java/com/nwmsu/topicoftheDay2A/TopicoftheDay2AApplication.java
+++ src/main/java/com/nwmsu/topicoftheDay2A/TopicoftheDay2AApplication.java
@@ -2,12 +2,16 @@ package com.nwmsu.topicoftheDay2A;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Controller
 @SpringBootApplication
 public class TopicoftheDay2AApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TopicoftheDay2AApplication.class, args);
+	}
+	@RequestMapping("/")
+	String index() {
+		return "index";
 	}
 
 }
