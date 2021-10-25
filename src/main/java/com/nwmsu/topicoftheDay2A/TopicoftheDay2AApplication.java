d559e5421ecfa076c5a348e0d5289265e22028b8
Sanjay-13 <69335575+Sanjay-13@users.noreply.github.com>
2021-10-25 02:05:41 -0500

    Added project files

--- src/main/java/com/nwmsu/topicoftheDay2A/TopicoftheDay2AApplication.java
+++ src/main/java/com/nwmsu/topicoftheDay2A/TopicoftheDay2AApplication.java
@@ -2,16 +2,30 @@ package com.nwmsu.topicoftheDay2A;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @SpringBootApplication
-public class TopicoftheDay2AApplication {
+public class TopicoftheDay2AApplication implements CommandLineRunner {
+	@Autowired
+    private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TopicoftheDay2AApplication.class, args);
 	}
-	@RequestMapping("/")
-	String index() {
-		return "index";
-	}
+	 @Override
+	    public void run(String... args) throws Exception {
+	        String sql = "INSERT INTO public.topics(\n"
+	        		+ "	\"Weekdays\", \"Nameofthetopic\")\n"
+	        		+ "	VALUES ('monday', 'Application frameworks')";
+	         
+	        int rows = jdbcTemplate.update(sql);
+	        if (rows > 0 ) {
+	            System.out.println("A new row has been inserted.");
+	        }
+	    }
 
 }
