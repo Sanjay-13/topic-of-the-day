df91ea7f0bbf62d0007a96a5e230042816091179
Sanjay-13 <69335575+Sanjay-13@users.noreply.github.com>
2021-10-31 17:30:17 -0500

    Updated the backend

---
+++ src/test/java/com/nwmsu/topicoftheDay2A/TopicoftheDay2AApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwmsu.topicoftheDay2A;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicoftheDay2AApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
