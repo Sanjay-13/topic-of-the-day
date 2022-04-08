38f0e979287646e3f1872d0803e1b746e21b2f59
Sanjay-13 <69335575+Sanjay-13@users.noreply.github.com>
2022-04-08 01:02:05 -0500

    deleted sql scripts

--- target/classes/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO public.topics(
-	id, description, topic_name)
-	VALUES (1, 'Enjoy a Movie', 'Movie Monday');
-INSERT INTO public.topics(
-	id, description, topic_name)
-	VALUES (2, 'Enjoy a Free Taco', 'Taco Tuesday');
