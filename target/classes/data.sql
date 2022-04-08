539753f8cc1f2ee5d47a32f5ad8482a52c4f4c40
Sanjay-13 <69335575+Sanjay-13@users.noreply.github.com>
2022-04-07 22:14:50 -0500

    adding database files

---
+++ target/classes/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.topics(
+	id, description, topic_name)
+	VALUES (1, 'Enjoy a Movie', 'Movie Monday');
+INSERT INTO public.topics(
+	id, description, topic_name)
+	VALUES (2, 'Enjoy a Free Taco', 'Taco Tuesday');
